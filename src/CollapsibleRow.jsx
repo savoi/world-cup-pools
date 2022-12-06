940520368d8e39b77c4154de39fc58b56f979556
Josh Greenberg <castro.greenberg@gmail.com>
2022-12-05 21:05:02 -0600

    Add missing dependency to package.json

--- src/CollapsibleRow.jsx
+++ src/CollapsibleRow.jsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
